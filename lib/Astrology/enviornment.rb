4007fbbcbe155aca7fecb3180f8799bbdf0a9806
Chelsea <croland711@yandex.com>
2020-12-17 12:39:26 -0800

    Cleaned up code

---
+++ lib/Astrology/enviornment.rb
@@ -0,0 +1,11 @@
+require_relative "version"
+require_relative "CLI"
+require_relative "API"
+require_relative "description"
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+
+require "httparty"
